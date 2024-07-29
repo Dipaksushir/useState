451f434498c0e82266f9b4e9ec6bfd7cdb1693ba
Dipak Sushir <dipaksushir2000@gmail.com>
2024-07-29 12:36:15 +0530

    usestatehook

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const[counter,setCounter]=useState(0)
+  const[name,setName]=useState('Sevenmentor')
+  function minus()
+  {
+    if(counter>0)
+      {
+        setCounter(counter-1)
+      }
+  }
+  function chngname()
+  {
+    setName("I.T. Education")
+  }
+  return (
+    <div className="App">
+      <h1>UseState Hook</h1>
+      <h2>Counter: {counter}</h2>
+      <h3>{name}</h3>
+      <button onClick={()=>setCounter(counter+1)}>Increment</button>
+      <button onClick={()=>minus()}>Decrement</button><br></br>
+      <button onClick={()=>chngname()}>Change Name</button>
+    </div>
+  );
+}
+
+export default App;
